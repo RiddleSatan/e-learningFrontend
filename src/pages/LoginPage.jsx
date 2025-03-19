a2406aff00aa917637912ddc20e779730a68ed06
Om Pandey <mynameisompandey007@gmail.com>
2025-03-19 23:38:12 +0530

    some changes

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -38,8 +38,8 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex flex-col rounded-md">
-      <main className="flex-grow flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 rounded-md">
+    <div className="min-h-screen bg-gray-900 text-white flex flex-col">
+      <main className="flex-grow flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
             <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
@@ -59,13 +59,13 @@ const LoginPage = () => {
                   type="email"
                   autoComplete="email"
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-white rounded-md sm:text-sm"
                   placeholder="Email address"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
-              <div>
+              <div className="mt-4">
                 <label htmlFor="password" className="sr-only">
                   Password
                 </label>
@@ -75,7 +75,7 @@ const LoginPage = () => {
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  className="appearance-none relative block w-full px-3 py-2 border text-white border-gray-300 placeholder-gray-500  rounded-md sm:text-sm" 
                   placeholder="Password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
@@ -86,7 +86,7 @@ const LoginPage = () => {
             <div>
               <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
               >
                 Sign in
               </button>
@@ -94,7 +94,7 @@ const LoginPage = () => {
           </form>
         </div>
       </main>
-   <Footer/>
+      <Footer />
     </div>
   );
 };
